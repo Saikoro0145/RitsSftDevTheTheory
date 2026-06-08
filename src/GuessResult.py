7c9e7550a33617712e4381e6fdfd2f2f3ec135ed
Saikoro <yg.hayashi0145@gmail.com>
2026-06-08 17:38:44 +0900

    feat: GuessResult.py を作成

---
+++ src/GuessResult.py
@@ -0,0 +1,10 @@
+class GuessResult:
+    def __init__(hits, blows):
+        self.hits = hits
+        self.blows = blows
+
+    def GetHits():
+        return self.hits
+
+    def GetBlows():
+        return self.blows
